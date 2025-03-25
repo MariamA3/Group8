0ff3dab00e4f8ade6fa79b38c27be251e2332eef
Jon P. <jonper@stud.ntnu.no>
2025-03-25 17:10:01 +0100

    Added "concurrently", new scripts, updated .gitignore

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const dotenv = require('dotenv');
 const app = require("./app");
 const connectDB = require('./config/db'); 
+const path = require('path');
 
 dotenv.config({ path: '.env' }); 
+
+// Serving the frontend build files.
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
 
 //debug
 console.log("MONGO_URI:", process.env.MONGO_URI); 
