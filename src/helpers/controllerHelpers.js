b038b104c9ef77aab054dd3f1410d9561c884b8f
MariamA3 <mariamab@stud.ntnu.no>
2025-03-25 17:07:32 +0100

    creating a helper folder, with code since a lot of the controllers are repeating the code

---
+++ src/helpers/controllerHelpers.js
@@ -0,0 +1,19 @@
+const Models = require('../models'); 
+
+
+//make helpers for the functions that are repeated a lot 
+
+//Get model 
+
+
+//Get model by id 
+
+
+//delete model 
+
+
+
+
+
+
+module.exports = helpers
