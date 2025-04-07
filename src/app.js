e79e8cb47b310e6b1f5376080001659a79f11f6d
Jon <jonper@stud.ntnu.no>
2025-04-07 14:04:06 +0200

    Re-serving the front-end in app.js, updated packages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require('dotenv')
 const dbConnection = require('./dbConnection');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,20 +11,26 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-/// Load environment variables from .env file
+// Serving the frontend build files.
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+});
+
+// Load environment variables from .env file
 dotenv.config();
 
-//db connection
-dbConnection()
+// db connection
+dbConnection();
 
-//for debugging
+// for debugging
 app.get("/", (req, res) => {
     res.send("Server is running!");
-  });  
+});
 
-
-//legg til routes her sp slipper vi mange linjer
-//'authRoutes', 'studyRoutes', <--remove dfor debugging app.js
+// legg til routes her sp slipper vi mange linjer
+// 'authRoutes', 'studyRoutes', <--remove dfor debugging app.js
 const routes = [ 'artefactRoutes', 'studyRoutes', 'invitationRoutes' ];
 routes.forEach(route => {
     app.use('/api', require(`./routes/${route}`));
